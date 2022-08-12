c1f1723f87695093dafc3b0f0c061466f59c3e73
ADITYA KARUNIADIKARA <aditya@cakeplabs.com>
2022-08-12 13:53:37 +0800

    add ENV

--- config/mysql.go
+++ config/mysql.go
@@ -2,13 +2,27 @@ package config
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 )
 
 func Connect() *sql.DB {
-	db, err := sql.Open("mysql", "b6e87f3c439c25:dd7a6506@(us-cdbr-east-06.cleardb.net)/heroku_c5002e639cb82ea")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Some error occured. Err: %s", err)
+	}
+
+	User := os.Getenv("DBUSER")
+	Pass := os.Getenv("DBPASS")
+	Server := os.Getenv("DBSERVER")
+	Database := os.Getenv("DB")
+	dsm := fmt.Sprintf("%s:%s@(%s)/%s", User, Pass, Server, Database)
+
+	db, err := sql.Open("mysql", dsm)
 
 	if err != nil {
 		log.Fatal(err)
