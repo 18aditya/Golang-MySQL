64322a6b5f813058f3231806b24e32e5a67525a8
ADITYA KARUNIADIKARA <aditya@cakeplabs.com>
2022-08-12 18:08:07 +0800

    JWT working

--- middleware/jwt.go
+++ middleware/jwt.go
@@ -3,7 +3,6 @@ package middleware
 import (
 	"net/http"
 	"os"
-	"strconv"
 
 	"github.com/dgrijalva/jwt-go"
 )
@@ -23,7 +22,7 @@ func ValidateJWT(next func(w http.ResponseWriter, r *http.Request)) http.Handler
 
 			if err != nil {
 				w.WriteHeader(http.StatusUnauthorized)
-				w.Write([]byte("not authorized: " + strconv.FormatBool(token.Valid)))
+				w.Write([]byte("not authorized: " + err.Error()))
 			}
 
 			if token.Valid {
