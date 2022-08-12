59d973efcc83ebc5a3fdbfd8b7b44c350148a59a
ADITYA KARUNIADIKARA <aditya@cakeplabs.com>
2022-08-12 17:41:07 +0800

    jwt bugfix

--- middleware/jwt.go
+++ middleware/jwt.go
@@ -9,7 +9,7 @@ import (
 
 func ValidateJWT(next func(w http.ResponseWriter, r *http.Request)) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.Header["Authorization"] != nil {
+		if r.Header["Token"] != nil {
 			token, err := jwt.Parse(r.Header["Token"][0], func(t *jwt.Token) (interface{}, error) {
 				_, ok := t.Method.(*jwt.SigningMethodHMAC)
 				if !ok {
