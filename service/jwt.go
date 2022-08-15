b5911487fc01ad23ee72920ef5f28d65ac1c6961
ADITYA KARUNIADIKARA <aditya@cakeplabs.com>
2022-08-15 16:35:28 +0800

    JWt with ID bugfix

--- service/jwt.go
+++ service/jwt.go
@@ -31,7 +31,7 @@ func CreateJWT(key string) (string, error) {
 }
 
 func GetJWT(w http.ResponseWriter, r *http.Request) {
-	var user model.Users
+	// var user model.Users
 	// var arr_user []model.Users
 
 	var (
@@ -43,41 +43,30 @@ func GetJWT(w http.ResponseWriter, r *http.Request) {
 	sql := fmt.Sprintf("Select * from Users Where IdUsers = %s", key)
 
 	if r.Header["Authorization"] != nil {
-		rows, err := db.Query(sql)
-		if null := rows.Scan(&user.Id, &user.First_name, &user.Last_name, &user.Email, &user.CreatedAt); null == nil || err != nil {
+		err := db.QueryRow(sql)
+		if err != nil {
 			response.Status = 404
 			response.Message = "user not found"
 			w.Header().Set("Content-Type", "application/json")
 			json.NewEncoder(w).Encode(response)
 
 		} else {
-			for rows.Next() {
-				if err := rows.Scan(&user.Id, &user.First_name, &user.Last_name, &user.Email, &user.CreatedAt); err != nil {
-					response.Status = 404
-					response.Message = fmt.Sprintf("%s", err)
+			token, err := CreateJWT(key)
+			if err != nil {
+				response.Status = 403
+				response.Message = "Cannot create token"
 
-					w.Header().Set("Content-Type", "application/json")
-					json.NewEncoder(w).Encode(response)
+				w.Header().Set("Content-Type", "application/json")
+				json.NewEncoder(w).Encode(response)
+			} else {
+				response.Status = 200
+				response.Message = fmt.Sprintf("%s", token)
 
-				} else {
-					token, err := CreateJWT(key)
-					if err != nil {
-						response.Status = 403
-						response.Message = "Cannot create token"
-
-						w.Header().Set("Content-Type", "application/json")
-						json.NewEncoder(w).Encode(response)
-					} else {
-						response.Status = 200
-						response.Message = fmt.Sprintf("%s", token)
-
-						w.Header().Set("Content-Type", "application/json")
-						json.NewEncoder(w).Encode(response)
-					}
-				}
+				w.Header().Set("Content-Type", "application/json")
+				json.NewEncoder(w).Encode(response)
 			}
-
 		}
+
 	} else {
 		response.Status = 402
 		response.Message = "Insert user Id"
