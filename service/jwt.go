6d09ece41a865c5578622e5533c20802d67859db
ADITYA KARUNIADIKARA <aditya@cakeplabs.com>
2022-08-15 14:36:21 +0800

    changes

--- service/jwt.go
+++ service/jwt.go
@@ -1,70 +1,70 @@
 package service
 
-// import (
-// 	"encoding/json"
-// 	"fmt"
-// 	"net/http"
-// 	"os"
-// 	"time"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"time"
 
-// 	"github.com/dgrijalva/jwt-go"
-// 	"main.go/model"
-// )
+	"github.com/dgrijalva/jwt-go"
+	"main.go/model"
+)
 
-// func CreateJWT() (string, error) {
-// 	token := jwt.New(jwt.SigningMethodHS256)
+func CreateJWT() (string, error) {
+	token := jwt.New(jwt.SigningMethodHS256)
 
-// 	claims := token.Claims.(jwt.MapClaims)
+	claims := token.Claims.(jwt.MapClaims)
 
-// 	claims["exp"] = time.Now().Add(time.Hour).Unix()
+	claims["exp"] = time.Now().Add(time.Hour).Unix()
 
-// 	tokenStr, err := token.SignedString([]byte(os.Getenv("SECRET")))
+	tokenStr, err := token.SignedString([]byte(os.Getenv("SECRET")))
 
-// 	if err != nil {
-// 		fmt.Println(err.Error())
-// 		return "", err
-// 	}
+	if err != nil {
+		fmt.Println(err.Error())
+		return "", err
+	}
 
-// 	return tokenStr, nil
-// }
+	return tokenStr, nil
+}
 
-// func GetJWT(w http.ResponseWriter, r *http.Request) {
-// 	// err := godotenv.Load()
-// 	var (
-// 		response model.Response
-// 	)
-// 	// if err != nil {
-// 	// 	response.Status = 502
-// 	// 	response.Message = "Something is wrong when generating your token"
+func GetJWT(w http.ResponseWriter, r *http.Request) {
+	// err := godotenv.Load()
+	var (
+		response model.Response
+	)
+	// if err != nil {
+	// 	response.Status = 502
+	// 	response.Message = "Something is wrong when generating your token"
 
-// 	// 	w.Header().Set("Content-Type", "application/json")
-// 	// 	json.NewEncoder(w).Encode(response)
-// 	// }
+	// 	w.Header().Set("Content-Type", "application/json")
+	// 	json.NewEncoder(w).Encode(response)
+	// }
 
-// 	if r.Header["Authorization"] != nil {
-// 		if r.Header["Authorization"][0] != os.Getenv("API_KEY") {
+	if r.Header["Authorization"] != nil {
+		if r.Header["Authorization"][0] != os.Getenv("API_KEY") {
 
-// 			response.Status = 401
-// 			response.Message = "Api Key is wrong"
+			response.Status = 401
+			response.Message = "Api Key is wrong"
 
-// 			w.Header().Set("Content-Type", "application/json")
-// 			json.NewEncoder(w).Encode(response)
-// 		} else {
-// 			token, err := CreateJWT()
-// 			if err != nil {
-// 				response.Status = 403
-// 				response.Message = fmt.Sprintf("Cannot create token %s", err)
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(response)
+		} else {
+			token, err := CreateJWT()
+			if err != nil {
+				response.Status = 403
+				response.Message = fmt.Sprintf("Cannot create token %s", err)
 
-// 				w.Header().Set("Content-Type", "application/json")
-// 				json.NewEncoder(w).Encode(response)
-// 			} else {
-// 				response.Status = 200
-// 				response.Message = fmt.Sprintf("%s", token)
+				w.Header().Set("Content-Type", "application/json")
+				json.NewEncoder(w).Encode(response)
+			} else {
+				response.Status = 200
+				response.Message = fmt.Sprintf("%s", token)
 
-// 				w.Header().Set("Content-Type", "application/json")
-// 				json.NewEncoder(w).Encode(response)
-// 			}
-// 		}
-// 	}
+				w.Header().Set("Content-Type", "application/json")
+				json.NewEncoder(w).Encode(response)
+			}
+		}
+	}
 
-// }
+}
